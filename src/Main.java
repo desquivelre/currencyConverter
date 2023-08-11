63c0a858cadbc6639751412774a8da3343b92dde
Diego Esquivel <desquivelreuni@hotmail.com>
2023-08-10 19:53:17 -0500

    Ajustes de Main para mostrar correctamente el Formulario.java

--- src/Main.java
+++ src/Main.java
@@ -3,51 +3,24 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         ValidationUtil validationUtil = new ValidationUtil();
-        Converter converter = new Converter();
-
-        String testDoubleInput = "121.65";
-
-        if (validationUtil.validateDouble(testDoubleInput)) {
-            double valueCurrency = validationUtil.parseDoubleFromString(testDoubleInput);
-            converter.convertSolesToEuros(valueCurrency);
-        }
-
         Formulario formulario = new Formulario();
-        formulario.ConvertirMonedas(validationUtil.parseDoubleFromString(testDoubleInput));
 
-        while(true) {
+        while (true) {
+            String opciones = (JOptionPane.showInputDialog(null, "Seleccione una opción de conversión ", "Menu", JOptionPane.QUESTION_MESSAGE, null, new Object[]{"Conversor de Moneda"}, "Seleccion")).toString();
 
-            String opciones = (JOptionPane.showInputDialog(null, "Seleccione una opción de conversión ", "Menu", JOptionPane.QUESTION_MESSAGE, null, new Object[] {"Conversor de Moneda"}, "Seleccion")).toString();
-
-            switch(opciones) {
+            switch (opciones) {
                 case "Conversor de Moneda":
                     String input = JOptionPane.showInputDialog("Ingresa la cantidad de dinero que deseas convertir: ");
-                    if(ValidarNumero(input) == true) {
-                        double Minput = Double.parseDouble(input);
-                        monedas.ConvertirMonedas(Minput);
 
-                        int respuesta=JOptionPane.showConfirmDialog(null,"¿Deseas realizar otra conversión?");
-                        if (JOptionPane.OK_OPTION == respuesta){
+                    if (validationUtil.validateDouble(input) == true) {
+                        double Minput = validationUtil.parseDoubleFromString(input);
+
+                        formulario.ConvertirMonedas(Minput);
+
+                        int respuesta = JOptionPane.showConfirmDialog(null, "¿Deseas realizar otra conversión?");
+
+                        if (JOptionPane.OK_OPTION == respuesta) {
                             System.out.println("Selecciona opción Afirmativa");
-                        }else{
-                            JOptionPane.showMessageDialog(null, "Programa terminado");
-                        }
-
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Valor inválido");
-                    }
-                    break;
-
-
-                case "Conversor de Temperatura":
-                    input = JOptionPane.showInputDialog("Ingresa el valor de la temperatura que deseas convertir ");
-                    if(ValidarNumero(input) == true) {
-                        double Minput = Double.parseDouble(input);
-                        temperatura.ConvertirTemperatura(Minput);
-
-                        int respuesta = 0;
-                        respuesta = JOptionPane.showConfirmDialog(null, "¿Desea continuar?");
-                        if((respuesta == 0) && (ValidarNumero(input) == true)) {
                         } else {
                             JOptionPane.showMessageDialog(null, "Programa terminado");
                         }
@@ -56,7 +29,8 @@ public class Main {
                         JOptionPane.showMessageDialog(null, "Valor inválido");
                     }
                     break;
-            }
 
+            }
         }
+    }
 }
